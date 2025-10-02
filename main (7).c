1693d95c76d8068b5c152ce875dbf8ebf5b0fd4b
Luiz26062009 <schultz.luiz@escola.pr.gov.br>
2025-10-02 12:11:11 -0300

    Add files via upload

---
+++ main (7).c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    int matriz[2][3], soma = 0;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("Digite o elemento [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+            soma += matriz[i][j];
+        }
+    }
+    printf("Soma dos elementos = %d\n", soma);
+    return 0;
+}
